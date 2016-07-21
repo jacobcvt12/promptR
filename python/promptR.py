ed1d8440e4daaa21fbe4cc0c5b94d67df53466d6
Jacob Carey <JCarey@telligen.com>
2016-07-21 12:34:24 -0400

    Add while loop

--- python/promptR.py
+++ python/promptR.py
@@ -1,9 +1,14 @@
 from __future__ import unicode_literals
 from prompt_toolkit import prompt
+from prompt_toolkit.history import InMemoryHistory
 
 def main():
-    text = prompt("> ")
-    print("You Entered:", text)
+    history = InMemoryHistory()
+
+    while True:
+        text = prompt("> ", history=history)
+        print("You Entered:", text)
+    print('GoodBye!')
 
 if __name__ == '__main__':
     main()

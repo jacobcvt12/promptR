b867bfbc5a46277c0439d2cc29534e66ffc88584
Jacob Carey <JCarey@telligen.com>
2016-07-21 12:39:26 -0400

    (Try) to color syntax for R

--- python/promptR.py
+++ python/promptR.py
@@ -1,12 +1,13 @@
 from __future__ import unicode_literals
 from prompt_toolkit import prompt
 from prompt_toolkit.history import InMemoryHistory
+from pygments.lexers import RConsoleLexer
 
 def main():
     history = InMemoryHistory()
 
     while True:
-        text = prompt("> ", history=history)
+        text = prompt("> ", lexer=RConsoleLexer, history=history)
         print("You Entered:", text)
     print('GoodBye!')
 
